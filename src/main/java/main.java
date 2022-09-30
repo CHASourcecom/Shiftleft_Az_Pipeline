1fa62d00b660a5b41df062c02b8c7793ed1d0df2
Clara <cha@sourcecom.se>
2022-09-30 12:54:36 +0200

    Added Junit

---
+++ src/main/java/main.java
@@ -0,0 +1,15 @@
+import org.junit.*;
+
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        String secret_key = "x4svoqxieG3DA14gKvIwJ5qTlB7GI0W+KaBRxfSO";
+
+    }
+    @Test
+    public void test(){
+        System.out.println("Test");
+    }
+
+}
